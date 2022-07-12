3ceb3fe2467124441168e3f2a74deb496b46aa4a
[Nico242001] <nthiv077@uottawa.ca>
2022-07-12 16:49:34 -0400

    popper

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '@popperjs/core'; // Edit here
+// import 'bootstrap/dist/js/bootstrap.bundle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
