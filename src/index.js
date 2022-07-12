90ef4d58efbb3475d7359e47fd95989a3b7d7cb7
[Nico242001] <nthiv077@uottawa.ca>
2022-07-12 16:37:41 -0400

    net

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
