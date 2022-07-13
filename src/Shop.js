50996bd2eb2f4e1b2aa567ce72cc0cc5da58bae1
[Nico242001] <nthiv077@uottawa.ca>
2022-07-13 01:20:14 -0400

    test

--- src/Shop.js
+++ src/Shop.js
@@ -4,32 +4,27 @@ import basketball from "./images/shop/basketball.jpg"
 import sale from "./images/shop/sale.jpg"
 
 const Shop = () => {
+    // const [imgHeight, set]
     return ( 
        <div className="shop">
-        <div className="row" min-width="100%">
-                <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding " min-width="50%" max-width="50%">
+        <div className="row btm-border" min-width="100%">
+                <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding icon" min-width="50%" max-width="50%">
                     <div className="container text-center text-white">
-                        <h2 className='fit mx-auto'>All New Adidas Cleats!!!</h2>
-                        <br/>
-                        <h4 className='fit mx-auto'>The all new Speed of Light Adidas X boots are finally out! Dont wait, get them now before we are all sold out!</h4>
-                        <br/>
-                        <button className="btn btn-secondary mx-auto">Buy Now!</button></div>
+                        <h2 className='fit mx-auto'>Shop Soccer Now!</h2>
+                        </div>
                 </div>
                 <div className="col padding" min-width="50%" max-width="50%">
-                       <img src={soccer} className="max-width"alt="" min-width="100%" />
+                       <img id="soccer" ref={ref} src={soccer} className="max-width"alt="" min-width="100%" />
                 </div>
             </div>
-            <div className="row" min-width="100%">
+            <div className="row btm-border" min-width="100%">
             <div className="col padding" min-width="50%" max-width="50%">
                        <img src={basketball} className="max-width"alt="" min-width="100%" />
                 </div>
                 <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding " min-width="50%" max-width="50%">
                     <div className="container text-center text-white">
-                        <h2 className='fit mx-auto'>All New Adidas Cleats!!!</h2>
-                        <br/>
-                        <h4 className='fit mx-auto'>The all new Speed of Light Adidas X boots are finally out! Dont wait, get them now before we are all sold out!</h4>
-                        <br/>
-                        <button className="btn btn-secondary mx-auto">Buy Now!</button></div>
+                    <h2 className='fit mx-auto'>Shop BasketBall Now!</h2>
+                    </div>
                 </div>
                 
             </div>
