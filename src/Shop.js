bed7810a5e8a8de7c7db0dbb04af38d22c07f45a
[Nico242001] <nthiv077@uottawa.ca>
2022-07-13 01:24:28 -0400

    bug

--- src/Shop.js
+++ src/Shop.js
@@ -8,10 +8,10 @@ const Shop = () => {
     return ( 
        <div className="shop">
         <div className="row btm-border" min-width="100%">
-                <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding icon" min-width="50%" max-width="50%">
+        <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding " min-width="50%" max-width="50%">
                     <div className="container text-center text-white">
-                        <h2 className='fit mx-auto'>Shop Soccer Now!</h2>
-                        </div>
+                    <h2 className='fit mx-auto'>Shop BasketBall Now!</h2>
+                    </div>
                 </div>
                 <div className="col padding" min-width="50%" max-width="50%">
                        <img id="soccer"  src={soccer} className="max-width"alt="" min-width="100%" />
@@ -28,7 +28,16 @@ const Shop = () => {
                 </div>
                 
             </div>
-        
+            <div className="row btm-border" min-width="100%">
+                <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding icon" min-width="50%" max-width="50%">
+                    <div className="container text-center text-white">
+                        <h2 className='fit mx-auto'>Shop Hockey Now!</h2>
+                        </div>
+                </div>
+                <div className="col padding" min-width="50%" max-width="50%">
+                       <img id="soccer"  src={hockey} className="max-width"alt="" min-width="100%" />
+                </div>
+            </div>
 
 
     </div>
