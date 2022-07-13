d37ff22ade0629ee837b2cf310fbf984d5403818
[Nico242001] <nthiv077@uottawa.ca>
2022-07-13 01:28:15 -0400

    hocjey

--- src/Shop.js
+++ src/Shop.js
@@ -10,7 +10,7 @@ const Shop = () => {
         <div className="row btm-border" min-width="100%">
         <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding " min-width="50%" max-width="50%">
                     <div className="container text-center text-white">
-                    <h2 className='fit mx-auto'>Shop BasketBall Now!</h2>
+                    <h2 className='fit mx-auto'>Shop Soccer Now!</h2>
                     </div>
                 </div>
                 <div className="col padding" min-width="50%" max-width="50%">
@@ -29,10 +29,10 @@ const Shop = () => {
                 
             </div>
             <div className="row btm-border" min-width="100%">
-                <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding icon" min-width="50%" max-width="50%">
+            <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding " min-width="50%" max-width="50%">
                     <div className="container text-center text-white">
-                        <h2 className='fit mx-auto'>Shop Hockey Now!</h2>
-                        </div>
+                    <h2 className='fit mx-auto'>Shop Hockey Now!</h2>
+                    </div>
                 </div>
                 <div className="col padding" min-width="50%" max-width="50%">
                        <img id="soccer"  src={hockey} className="max-width"alt="" min-width="100%" />
