12281577a15c34af7b33ebe70e4d8a7840febf28
[Nico242001] <nthiv077@uottawa.ca>
2022-07-13 01:09:42 -0400

    basketball

--- src/Shop.js
+++ src/Shop.js
@@ -1,6 +1,6 @@
 import soccer from "./images/shop/soccer.jpg"
 import hockey from "./images/shop/hockey.jpg"
-import basketball from "./images/shop/basketball.png"
+import basketball from "./images/shop/basketball.jpg"
 import sale from "./images/shop/sale.jpg"
 
 const Shop = () => {
@@ -18,6 +18,20 @@ const Shop = () => {
                 <div className="col padding" min-width="50%" max-width="50%">
                        <img src={soccer} className="max-width"alt="" min-width="100%" />
                 </div>
+            </div>
+            <div className="row" min-width="100%">
+            <div className="col padding" min-width="50%" max-width="50%">
+                       <img src={basketball} className="max-width"alt="" min-width="100%" />
+                </div>
+                <div className="col bg-dark ml-auto d-flex align-items-center mt-md-0 bottomBorder padding " min-width="50%" max-width="50%">
+                    <div className="container text-center text-white">
+                        <h2 className='fit mx-auto'>All New Adidas Cleats!!!</h2>
+                        <br/>
+                        <h4 className='fit mx-auto'>The all new Speed of Light Adidas X boots are finally out! Dont wait, get them now before we are all sold out!</h4>
+                        <br/>
+                        <button className="btn btn-secondary mx-auto">Buy Now!</button></div>
+                </div>
+                
             </div>
         
 <div className="container text-center mt-3">
