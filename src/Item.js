2c1eacb11ca79753ac23c8f5af98147a0fcfc6ef
[Nico242001] <nthiv077@uottawa.ca>
2022-07-18 11:43:16 -0400

    item

--- src/Item.js
+++ src/Item.js
@@ -7,11 +7,11 @@ let obj;
 let idInt;
 let itemLink;
 let list = [];
-function findObject(id){
+function findObject(id) {
     console.log("in", id, list);
-    for(let i=0;i<list.length;i++){
+    for (let i = 0; i < list.length; i++) {
         console.log(list[i].id);
-        if(id == list[i].id){
+        if (id == list[i].id) {
             console.log(list[i])
             return list[i]
         }
@@ -85,59 +85,66 @@ const Item = () => {
     return (
         <div className="item bg-light">
 
-            <div className="container-fluid pt-4">
-
-                <div className="container border border-3 border-dark bg-light">
-                    <div>
-                        <h5><Link to="/">Home</Link> / <Link to="/shop">Shop</Link> / <Link to={"/sport/"+sport}>{sport}</Link> / <Link to={"/items/"+itemLink}>{item}</Link> / <Link to={"/item/"+obj.id}>{obj.name}</Link></h5>
-                    </div>
-                    <div className="row mx-auto">
-                        <div className="col p-0">
-                            <h2 className="text-left py-2 fit max-width">{obj.name}</h2>
+            <div className='container-fluid'>
+                <div className="container-fluid pt-4">
+                    <div className="container border border-3 border-dark bg-light">
+                        <div>
+                            <h5><Link to="/">Home</Link> / <Link to="/shop">Shop</Link> / <Link to={"/sport/" + sport}>{sport}</Link> / <Link to={"/items/" + itemLink}>{item}</Link> / <Link to={"/item/" + obj.id}>{obj.name}</Link></h5>
+                        </div>
+                        <div className="row mx-auto">
+                            <div className="col p-0">
+                                <h2 className="text-left py-2 fit max-width">{obj.name}</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="container m-4 p-0">
-        <div id="itemDescription" className="container float-start text-center border border-3 border-dark bg-light" max-width="60%">
-            <img id="image" src={obj.image} width="35%"/>
-            <br/>
-            <h3>Description</h3>
-            <p>{obj.description}</p>
-        </div>
-        <div id="details" className="container float-end border border-3 border-dark bg-light fit" max-width="35%">
-            <div class="container text-center border-bottom border-3 border-dark">
-                <h3 >Total: $<span id="price">{obj.price}</span></h3>
+                <div className="row">
+                    <div className="col">
+                        <div id="itemDescription" className="container-fluid border border-3 border-dark bg-light">
+                            <img id="image" src={obj.image} width="500px" />
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div id="details" className="container border border-3 border-dark bg-light fit">
+                            <div class="container text-center border-bottom border-3 border-dark">
+                                <h3 >Total: $<span id="price">{obj.price}</span></h3>
+                            </div>
+                            <div class="container mt-3">
+                                <h4 >Size</h4>
+                                <select name="itemSize" id="itemSize" width="85px" margin-left="25px"></select>
+                                <br />
+                                <br />
+                                <h4 >Quantity</h4>
+                                <select name="itemAmount" id="itemAmount" width="85px" margin-left="25px">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                </select>
+                                <br />
+                                <br />
+                                <button type="button" margin-left="25px">Add to Cart</button>
+                                <br />
+                                <br />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
-         
-            <div class="container mt-3">
-                <h4 >Size</h4>
-                <select name="itemSize" id="itemSize" width="85px" margin-left="25px"></select>
-                <br/>
-                <br/>
-                <h4 >Quantity</h4>
-                <select name="itemAmount" id="itemAmount" width="85px" margin-left="25px">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                </select>
-                <br/>
-                <br/>
-                <button type="button"  margin-left="25px">Add to Cart</button>
-                <br/>
-                <br/>
-            </div>
+
+
+
         </div>
-    </div>
-        </div>
+
     );
 }
 
