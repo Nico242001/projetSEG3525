cb40f0f4b33321a0b120fbcd0e1e8ebcff79bc47
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 15:13:57 -0400

    board

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,6 @@
-import { FixedSizeList as List } from 'react-window';
-
 const Board = () => {
     return ( <div className="board bg-light">
-<List
-    height={150}
-    itemCount={10}
-    itemSize={35}
-    width={300}
-  >
-    hey
-  </List>
+<div class="overflow-auto dialog" >HEDSOIjd osafosafiusadnf piosdanfipuansifpnsd pifnipsdanfvipnsda pifvndpsinipdsn ipcnsdpv nipdsnvi ods nvisdnv iosn iovnsdpionvo dnvn vnsdu v  ipoudsnv</div>
 
     </div> );
 }
