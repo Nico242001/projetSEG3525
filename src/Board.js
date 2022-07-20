0676da1d0d51980e246db65f82faf0741881ddca
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 15:56:44 -0400

    test

--- src/Board.js
+++ src/Board.js
@@ -3,17 +3,17 @@ const Board = () => {
     return (<div className="board bg-light">
         <div className="container text-center"><h1 className="mx-auto text=center">Reviews</h1></div>
         <div className="container mx-auto">
-            <div class="overflow-auto mx-auto dialog" >
+            <div class="overflow-auto mx-auto border border-3 border-dark dialog" >
                 {list.map((element) => (
 
-                    <p>{element}</p>
+                    <p className="p-2 bg-dark white">{element}</p>
                 ))
 
                 }
 
             </div>
             <div className="border border-3 border-dark mx-auto rev">
-                <input type="text" className="input-board"></input> <button type="button" className="btn btn-primary btn-color">Send</button>
+                <input type="text" className="input-board"></input> <button type="button" className="btn btn-secondary">Send</button>
             </div>
         </div>
     </div>);
