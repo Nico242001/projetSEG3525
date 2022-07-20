a60ff2d9a581ce2b8922a03c565fda6a2fe941cb
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 12:49:58 -0400

    faq

--- src/Faq.js
+++ src/Faq.js
@@ -2,16 +2,16 @@ const Faq = () => {
     return (  <div className="faq bg-light">
 
     <div className="container">
-    <div className="container padding-left"><h1 className="mx-auto text=center">FAQ</h1></div>
-        <div className="container text-center">
+    {/* <div className="container padding-left"><h1 className="mx-auto text=center">FAQ</h1></div> */}
+        <div className="container fit">
             Q: What sport items do you offer?<br/>
             A: We specialize in items for Soccer, Hockey and Basketball.
         </div>
-        <div className="container">
+        <div className="container fit">
             Q: Do you ship internationally?<br/>
             A: We currently exclussivly ship in Canada but are currently working on expanding to shipping internationally.
         </div>
-        <div className="container">
+        <div className="container fit">
             Q: What is your return policy?<br/>
             A: We have a 30 day return policy, no questions asked.
         </div>
