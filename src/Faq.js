5b29bb6d287043c593fcbcc00974704d70fde493
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 13:56:00 -0400

    faq

--- src/Faq.js
+++ src/Faq.js
@@ -3,19 +3,19 @@ const Faq = () => {
     <div className="row mt-3 mb-3"><div className="col-11 text-center"><h1 >FAQ</h1></div><div className="col-1"><h3>Fr</h3></div></div>
     <div className="container">
 
-        <div className="container border border-3 border-black fit margin-left mb-3 mt-3 bold faq-box">
+        <div className="container border border-3 border-dark  margin-left mb-3 mt-3 bold faq-box p-3" width="600px">
             Q: What sport items do you offer?<br/>
             A: We specialize in items for Soccer, Hockey and Basketball.
         </div>
-        <div className="container border border-3 border-dark fit margin-left mb-3 mt-3 bold faq-box p-3">
+        <div className="container border border-3 border-dark margin-left mb-3 mt-3 bold faq-box p-3" width="600px">
             Q: Do you ship internationally?<br/>
             A: We currently exclussivly ship in Canada but are currently working on expanding to shipping internationally.
         </div>
-        <div className="container border border-3 border-dark fit margin-left mb-3 mt-3 bold faq-box p-3">
+        <div className="container border border-3 border-dark margin-left mb-3 mt-3 bold faq-box p-3" width="600px">
             Q: What is your return policy?<br/>
             A: We have a 30 day return policy, no questions asked.
         </div>
-        <div className="container border border-3 border-dark fit margin-left mb-3 mt-3 bold faq-box p-3">
+        <div className="container border border-3 border-dark margin-left mb-3 mt-3 bold faq-box p-3" width="600px">
             Q: Where are you located?<br/> 
             A: We are located in Ottawa but exclussivly sell on our online store.
         </div>
