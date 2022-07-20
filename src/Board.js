ea5f96166c997c194862c84f96a6743a9100d6d8
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 15:17:02 -0400

    test

--- src/Board.js
+++ src/Board.js
@@ -1,8 +1,11 @@
 const Board = () => {
-    return ( <div className="board bg-light">
-<div class="overflow-auto dialog" >HEDSOIjd osafosafiusadnf piosdanfipuansifpnsd pifnipsdanfvipnsda pifvndpsinipdsn ipcnsdpv nipdsnvi ods nvisdnv iosn iovnsdpionvo dnvn vnsdu v  ipoudsnv</div>
+    return (<div className="board bg-light">
+        <div className="container text-center">
+            <div class="overflow-auto dialog" >HEDSOIjd osafosafiusadnf piosdanfipuansifpnsd pifnipsdanfvipnsda pifvndpsinipdsn ipcnsdpv nipdsnvi ods nvisdnv iosn iovnsdpionvo dnvn vnsdu v  ipoudsnv</div>
+        </div>
 
-    </div> );
+
+    </div>);
 }
- 
+
 export default Board;
