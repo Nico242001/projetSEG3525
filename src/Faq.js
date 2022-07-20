3bf6e5b18dc4ddb5c6cf126b01ccfaf26dea72ae
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 13:01:51 -0400

    faq

--- src/Faq.js
+++ src/Faq.js
@@ -3,19 +3,19 @@ const Faq = () => {
 
     <div className="container">
     {/* <div className="container padding-left"><h1 className="mx-auto text=center">FAQ</h1></div> */}
-        <div className="fit">
+        <div className="container margin-left">
             Q: What sport items do you offer?<br/>
             A: We specialize in items for Soccer, Hockey and Basketball.
         </div>
-        <div className="fit">
+        <div className="container margin-left">
             Q: Do you ship internationally?<br/>
             A: We currently exclussivly ship in Canada but are currently working on expanding to shipping internationally.
         </div>
-        <div className="fit">
+        <div className="container margin-left">
             Q: What is your return policy?<br/>
             A: We have a 30 day return policy, no questions asked.
         </div>
-        <div className="fit">
+        <div className="container margin-left">
             Q: Where are you located?<br/>
             A: We are located in Ottawa but exclussivly sell on our online store.
         </div>
