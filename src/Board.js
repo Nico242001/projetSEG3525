315b54bfedd6802b9162b2ca5e7128a2fb352121
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 15:36:45 -0400

    board

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,13 @@
 const Board = () => {
     return (<div className="board bg-light">
-        <div className="container text-center">
-            <div class="overflow-auto dialog" >HEDSOIjd osafosafiusadnf piosdanfipuansifpnsd pifnipsdanfvipnsda pifvndpsinipdsn ipcnsdpv nipdsnvi ods nvisdnv iosn iovnsdpionvo dnvn vnsdu v  ipoudsnv</div>
+        <div className="container text-center mx-auto">
+            <div class="overflow-auto dialog" >
+
+            </div>
+            <div className="border border-3 border-dark mx-auto dialog">
+
+            </div>
         </div>
-
-
     </div>);
 }
 
