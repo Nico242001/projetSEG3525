6b82b5a11b284485d5ea0f1812cb55b158d73012
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 15:50:45 -0400

    test

--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,8 @@ const Board = () => {
     let list=["I love my new Soccer ball!", "Ordered my new Hockey stick and it arrived the next day!", "My cleats broke and had no issue returning the pair and getting a new one!",]
     return (<div className="board bg-light">
         <div className="container text-center"><h1 className="mx-auto text=center">Reviews</h1></div>
-        <div className="container text-center mx-auto">
-            <div class="overflow-auto dialog" >
+        <div className="container mx-auto">
+            <div class="overflow-auto mx-auto dialog" >
                 {list.map((element) => (
 
                     <p>{element}</p>
