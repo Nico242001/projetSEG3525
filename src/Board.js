4cdb62e1a1d78e2800a89289a008fd5f06770060
[Nico242001] <nthiv077@uottawa.ca>
2022-07-20 16:17:31 -0400

    test

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,7 @@
 const Board = () => {
     let list = ["I love my new Soccer ball!", "Ordered my new Hockey stick and it arrived the next day!",
         "My cleats broke and had no issue returning the pair and getting a new one!", "My kids love the new Basketball they got for Christmas!",
-        "My son loves the new basketball I got him, he never wants to take it off!"]
+        "My son loves the new basketball shirt I got him, he never wants to take it off!", "I love going skating in the new skates I bought last week."]
     return (<div className="board bg-light">
         <div className="container text-center"><h1 className="mx-auto text=center">Reviews</h1></div>
         <div className="container mx-auto">
@@ -13,7 +13,7 @@ const Board = () => {
                 }
             </div>
             <div className="border border-3 border-dark mx-auto rev mt-2">
-                <input type="text" className="input-board"></input> <button type="button" className="btn btn-secondary">Send</button>
+                <input type="text" className="input-board"></input> <button type="button" className="btn btn-secondary btn-color">Send</button>
             </div>
         </div>
     </div>);
